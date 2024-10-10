f439fb683109ad514d338d972ef9a15bb16dcf48
Rolucatelli <rolucatelli@gmail.com>
2024-10-10 16:17:11 -0300

    adicao dos projetos do computador da faculdade

---
+++ Arquivos pc facul/TUS-2030/src/test/java/com/example/tus2030/Tus2030ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tus2030;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tus2030ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
